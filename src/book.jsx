10c8880a52164366aea5bf358c3f295dc743bf6d
elliotawe <elliot.awe@afrilogicsolutions.com>
2024-02-02 20:23:01 +0000

    book.jsx and bookshop.jsx added

---
+++ src/book.jsx
@@ -0,0 +1,16 @@
+export default function Book() {
+    return (
+        <>
+            <main className="px-4">
+                <ul>
+                    <ol>77 Ways get to more customers By: <i>Ubuy</i></ol>
+                    <ol>Authenticity By: <i>Emanuel Rose</i> </ol>
+                    <ol> Change Your thinking change your life By: <i>Brian Tracy</i></ol>
+    
+                👉 <a href="bookshop" className="text-blue-600 inline-block px-4 underline">see bookshop</a>
+                    {/* <a href="publisher/itemId" className="text-blue-600 underline">Publisher</a> */}
+                </ul>
+            </main>
+        </>
+    )
+    }
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,24 @@
+//main.jsx
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css';
 
-import {
+import 
+{
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
   Route
 } from 'react-router-dom';
+import Book from './book';
+import Bookshop from './bookshop';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-      <Route path='/' element={<p className='text-blue-700'>Hello, world</p>}></Route>
+      <Route path='/' element={<Book />}></Route>
+      <Route path='bookshop' element={<Bookshop />}></Route>
     </Route>
   )
 )
